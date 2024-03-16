76833b1fc6af0335a5faf84e7ae9bf635e1b6ea4
ju-dev-17 <jahidudd65@gmail.com>
2024-03-16 13:06:10 +0100

    init

---
+++ src/main/java/de/ju/springsecurityjwttemplate/SpringSecurityJwtTemplateApplication.java
@@ -0,0 +1,13 @@
+package de.ju.springsecurityjwttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityJwtTemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/ju/springsecurityjwttemplate/SpringSecurityJwtTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ju.springsecurityjwttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
